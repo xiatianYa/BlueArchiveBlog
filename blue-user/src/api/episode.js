b376695733a87b0100d4f3a6ee628895e7ba6811
xiatian <939648675@qq.com>
2024-04-02 22:05:45 +0800

    新增番剧集和番剧管理联动

---
+++ blue-user/src/api/episode.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询番剧集存储列表
+export function listEpisode(query) {
+  return request({
+    url: '/system/episode/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询番剧集存储详细
+export function getEpisode(id) {
+  return request({
+    url: '/system/episode/' + id,
+    method: 'get'
+  })
+}
+
+// 新增番剧集存储
+export function addEpisode(data) {
+  return request({
+    url: '/system/episode',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改番剧集存储
+export function updateEpisode(data) {
+  return request({
+    url: '/system/episode',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除番剧集存储
+export function delEpisode(id) {
+  return request({
+    url: '/system/episode/' + id,
+    method: 'delete'
+  })
+}
