ba0281c77b45cf431bc74b41298a9679826e476d
xiatian <939648675@qq.com>
2024-04-21 17:14:36 +0800

    引入ElasticSearch8.8.1配置...

---
+++ blue-modules/blue-system/src/main/java/com/blue/elastic/config/ElasticSearchConfig.java
@@ -0,0 +1,35 @@
+package com.blue.elastic.config;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import lombok.Data;
+import org.apache.http.HttpHost;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Data
+public class ElasticSearchConfig {
+    @Value("${spring.elasticsearch.uris}")
+    private String uris;
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+        String[] split = uris.split(":");
+        String hostname= split[0];
+        int port = Integer.parseInt(split[1]);
+        // 基本的用户名密码认证
+        BasicCredentialsProvider basicCredentialsProvider = new BasicCredentialsProvider();
+        RestClientBuilder restClientBuilder = RestClient.builder(new HttpHost(hostname, port, "http"));
+        restClientBuilder.setHttpClientConfigCallback(httpAsyncClientBuilder ->
+                httpAsyncClientBuilder.setDefaultCredentialsProvider(basicCredentialsProvider));
+        RestClient restClient = restClientBuilder.build();
+        ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+        return new ElasticsearchClient(transport);
+    }
+}
