f1f15a869e967ee2d6a8235157887b927be73164
xiatian <939648675@qq.com>
2024-04-22 17:03:07 +0800

    RabbitMq模块重构...

---
+++ blue-modules/blue-rabbitmq/src/main/java/com/blue/rabbitmq/aliyun/AliYunSmsService.java
@@ -0,0 +1,34 @@
+package com.blue.rabbitmq.aliyun;
+
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.profile.DefaultProfile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AliYunSmsService {
+    @Value("${aliyun.sms.AccessKeyId}")
+    private String AccessKeyId;
+    @Value("${aliyun.sms.Secret}")
+    private String Secret;
+    @Value("${aliyun.sms.SignName}")
+    private String SignName;
+    @Value("${aliyun.sms.TemplateCode}")
+    private String TemplateCode;
+    public void sendSms(String phone,String phoneCode) throws ClientException {
+        String regionId = "cn-shanghai";
+        DefaultProfile profile = DefaultProfile.getProfile(regionId,AccessKeyId,Secret);
+        IAcsClient client = new DefaultAcsClient(profile);
+        SendSmsRequest request = new SendSmsRequest();
+        request.setSignName(SignName);
+        request.setTemplateCode(TemplateCode);
+        request.setPhoneNumbers(phone);
+        request.setTemplateParam("{code:"+ phoneCode +"}");
+        client.getAcsResponse(request);
+    }
+}
+
+
